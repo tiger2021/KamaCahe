bf63e82b564aa574ae496a351b4ec1d270bda264
wangsht <wangsht@LAPTOP-80UB9QI1>
2025-10-29 14:56:15 +0800

    完成了缓存项目的初步框架搭建

---
+++ KamaCahe/KamaCaheDll/KamaCahePolicy.h
@@ -0,0 +1,16 @@
+#pragma once
+    template <typename Key, typename Value>
+    class KamaCachePolicy
+    {
+    public:
+        virtual ~KamaCachePolicy() {};
+
+        // 添加缓存接口
+        virtual void put(Key key, Value value) = 0;
+
+        // key是传入参数  访问到的值以传出参数的形式返回 | 访问成功返回true
+        virtual bool get(Key key, Value& value) = 0;
+        // 如果缓存中能找到key，则直接返回value
+        virtual Value get(Key key) = 0;
+
+    };
---
+++ KamaCahe/KamaCaheDll/LRUNode.h
@@ -0,0 +1,50 @@
+#pragma once
+
+#include <iostream>
+
+	template<typename Key, typename Value> 
+	class KLRUCache;
+	template <typename Key, typename Value>
+	class LRUNode
+	{
+	public:
+		LRUNode() {};
+		LRUNode(Key key, Value value) :m_key(key), m_value(value), m_accessCount(1)
+		{};
+		~LRUNode() {};
+
+		Key getKey() const
+		{
+			return m_key;
+		}
+
+		Value getValue() const
+		{
+			return m_value;
+		}
+
+		void setValue(Value value)
+		{
+			m_value = value;
+		}
+
+		size_t getAccessCount() const
+		{
+			return m_accessCount;
+		}
+
+		void incrementAccessCount()
+		{
+			++m_accessCount;
+		}
+
+	private:
+		Key	m_key;
+		Value m_value;
+		size_t m_accessCount;
+		std::weak_ptr<LRUNode<Key, Value>> m_pre; //std::weak_ptr智能指针，防止循环引用
+		std::weak_ptr<LRUNode<Key, Value>> m_next;
+
+		//友元类 能够访问private成员,protected成员和public成员
+		friend class KLRUCache<Key, Value>;
+	};		
